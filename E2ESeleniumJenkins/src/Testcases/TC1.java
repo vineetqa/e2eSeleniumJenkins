afd76cbfeb8773dc23b33144acdb973c139e0607
Alka <Alka@192.168.2.12>
2017-08-09 19:00:01 -0400

    Automation project

---
+++ E2ESeleniumJenkins/src/Testcases/TC1.java
@@ -0,0 +1,18 @@
+package Testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC1 {
+	@Test
+	public void testcase1()
+	{    System.setProperty("wedriver.chrome.driver","C:\\Users\\Alka\\workspace\\E2ESeleniumJenkins\\drivers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("Hello");
+		driver.quit();
+	}
+
+}
